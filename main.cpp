4e958918d8018128918f52a5b3d979d5edb3d6b3
BourgondAries <macocio@gmail.com>
2015-08-29 12:02:46 +0200

    Complete the cypher by easily transferring from raw text

--- main.cpp
+++ main.cpp
@@ -3,6 +3,58 @@
 #include <iostream>
 #include <random>
 
+char fromBitMessageCharacter(const std::vector<bool> &character)
+{
+	char to_return = 0b00000000;
+	for (int iterator = 0; iterator < 8; ++iterator)
+		to_return |= (0b00000001 << iterator) & (static_cast<char>(character[iterator]) << iterator);
+
+	return to_return;
+}
+
+std::string fromBitMessage(std::vector<bool> message)
+{
+	std::string to_return;
+	while (message.empty() == false)
+	{
+		std::vector<bool> eight_bits;
+		for (int iterator = 0; iterator < 8; ++iterator)
+		{
+			eight_bits.emplace_back(message.at(0));
+			message.erase(message.begin());
+		}
+		to_return.push_back(fromBitMessageCharacter(eight_bits));
+	}
+	return to_return;
+}
+
+std::vector<bool> toBitMessage(const char &message)
+{
+	std::vector<bool> bit_string;
+	bit_string.emplace_back(message & 0b00000001);
+	bit_string.emplace_back(message & 0b00000010);
+	bit_string.emplace_back(message & 0b00000100);
+	bit_string.emplace_back(message & 0b00001000);
+
+	bit_string.emplace_back(message & 0b00010000);
+	bit_string.emplace_back(message & 0b00100000);
+	bit_string.emplace_back(message & 0b01000000);
+	bit_string.emplace_back(message & 0b10000000);
+	return bit_string;
+}
+
+std::vector<bool> toBitMessage(const std::string &message)
+{
+	std::vector<bool> bit_string;
+	for (char character : message)
+	{
+		std::vector<bool> character_bits = toBitMessage(character);
+		for (bool bit : character_bits)
+			bit_string.emplace_back(bit);
+	}
+	return bit_string;
+}
+
 int main()
 {
 	std::mt19937_64 engine(std::time(nullptr)); // Generate a key
@@ -16,12 +68,8 @@ int main()
 	{
 		engine = std::mt19937_64(key); // Seed the engine with the key
 
-		std::vector<bool> message // Create the ASCII message "abc", LSB first.
-		{
-			1, 0, 0, 0, 0, 1, 1, 0,
-			0, 1, 0, 0, 0, 1, 1, 0,
-			1, 1, 0, 0, 0, 1, 1, 0,
-		};
+		std::vector<bool> message = toBitMessage("Hello World!"); // Chop a message up into bits
+		std::cout << fromBitMessage(message);
 
 		for (bool bit : message)
 		{
@@ -38,6 +86,7 @@ int main()
 			if (++counter % 8 == 0)
 				std::cout << '\n';
 		}
+		std::cout << fromBitMessage(encrypted_message) << '\n';
 	}
 
 	std::cout << '\n';
@@ -65,5 +114,6 @@ int main()
 			if (++counter % 8 == 0)
 				std::cout << '\n';
 		}
+		std::cout << fromBitMessage(decrypted_message) << '\n';
 	}
 }
